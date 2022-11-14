8498e47bef56f8ec3a07dd50fd8c35d4ca150535
Alvaro Tapia <alvartap@gmail.com>
2022-11-14 23:46:59 +0100

    new: search improved

--- app/src/main/java/com/antoniosep/createtestapp/SearchForm.java
+++ app/src/main/java/com/antoniosep/createtestapp/SearchForm.java
@@ -2,15 +2,43 @@ package com.antoniosep.createtestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class SearchForm extends AppCompatActivity
 {
 
     Button searchButton2;
+    EditText editTextTitleText;
+
+    @SuppressLint("Range")
+    public void search()
+    {
+        String titulo = editTextTitleText.getText().toString().trim().toLowerCase();
+
+        if(titulo.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(), "Introduzca un título para buscar", Toast.LENGTH_SHORT).show();
+            hideSoftKeyboard(editTextTitleText);
+        }
+        else
+        {
+            hideSoftKeyboard(editTextTitleText);
+        }
+    }
+
+    private void hideSoftKeyboard(View v) {
+        InputMethodManager inputMethodManager;
+        inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,10 +47,16 @@ public class SearchForm extends AppCompatActivity
         setContentView(R.layout.search_form);
 
         searchButton2 = (Button) findViewById(R.id.searchButton2);
+        editTextTitleText = (EditText) findViewById(R.id.editTextTitleText);
     }
 
     public void onClick(View view)
     {
-        Toast.makeText(getApplicationContext(), "pito", Toast.LENGTH_SHORT).show();
+        switch (view.getId())
+        {
+            case R.id.searchButton2: search(); break;
+        }
     }
+
+
 }
